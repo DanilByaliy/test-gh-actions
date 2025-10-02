fe854541e551df7e7aa35b4636e8f3cc593d1809
DanilByaliy <byaliy03@gmail.com>
2025-10-02 19:12:48 +0300

    test gpt

--- whitepace/vite.config.js
+++ whitepace/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/test-gh-actions/",
+  base: "/test-gh-actions/whitepace",
   // server: {
   //   open: true,
   //   port: 3001,
