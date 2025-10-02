e8bfc88a365f627593fb82995bf2697ccd8628d4
DanilByaliy <byaliy03@gmail.com>
2025-10-02 19:38:32 +0300

    fix path

---
+++ honey/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/test-gh-actions/honey",
+  // server: {
+  //   open: true,
+  //   port: 3001,
+  // },
+});
